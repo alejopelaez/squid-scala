ba82995d8ad319054acdff63273b595528b8ca41
Alejandro Pelaez <alejopelaezr@gmail.com>
2012-06-09 14:47:37 -0500

    Added scalatest

---
+++ src/test/SquidNodeTest.scala
@@ -0,0 +1,15 @@
+package test
+
+import org.scalatest.FunSuite
+
+/**
+ * User: Alejandro Peláez
+ * Date: 9/06/12
+ * Time: 01:44 PM
+ */
+
+class SquidNodeTest extends FunSuite{
+  test("Should receive message that belongs to itself")(pending)
+  test("Should not receive message that do not belongs to itself")(pending)
+  test("Should route messages correctly")(pending)
+}
